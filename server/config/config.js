f8bdc51d0f3b81593132a57030aed6422c825dda
michael byalsky <michael.byalsky@gmail.com>
2020-12-25 07:34:52 +0200

    gcloud5

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || null,
     database: process.env.DB_TEST || 'invests_test',
-    host: process.env.DB_HOST || null,
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: 'mysql',
     define: { underscored: true },
     logging: false,
